643086dda21cbd0043e8a4077fb9c19fe4645505
Mark Odayan <markodayansa@gmail.com>
2022-08-03 00:23:33 +0200

    patricia binary encoding prep

--- src/lib/data-structures/tries/patricia-trie.ts
+++ src/lib/data-structures/tries/patricia-trie.ts
@@ -2,6 +2,34 @@
 
 // A patricia trie has nodes that can contain a maximum of 2 direct children
 
+// Encode ASCII string into binary string
+function binary(str: string) {
+  const zeroPad = (num: string) => {
+    return '00000000'.slice(String(num).length) + num;
+  };
+
+  return str.replace(/[\s\S]/g, function (str: any) {
+    str = zeroPad(str.charCodeAt().toString(2));
+    return str;
+  });
+}
+
+// Decode binary string into ASCII string
+function ascii(str: string) {
+  // Removes the spaces from the binary string
+  str = str.replace(/\s+/g, '');
+  // Pretty (correct) print binary (add a space every 8 characters)
+  str = str.match(/.{1,8}/g)!.join(' ');
+
+  var binString = '';
+
+  str.split(' ').map(function (bin: string) {
+    binString += String.fromCharCode(parseInt(bin, 2));
+  });
+
+  return binString;
+}
+
 interface INode {
   end: boolean;
   children: IChildrenMap;
