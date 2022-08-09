2d6fee6c92a3152f24d5502b0f42b488a24a5be5
Mark Odayan <markodayansa@gmail.com>
2022-08-09 15:01:18 +0200

    rlp encode method complete

--- src/lib/serialisation/rlp.ts
+++ src/lib/serialisation/rlp.ts
@@ -18,8 +18,8 @@ function isEscapedFormat(input: Input): boolean {
   return false;
 }
 
-function getByteSize(input: string | Array<Input>): number {
-  return input.length;
+function hexToHexArray(input: string): string[] {
+  return input.length % 2 === 0 ? input.match(/.{1,2}/g)! : ('0' + input).match(/.{1,2}/g)!;
 }
 
 function encode(input: Input): Uint8Array {
@@ -47,10 +47,9 @@ function encode(input: Input): Uint8Array {
     }
   } else if (typeof input === 'string' && input.startsWith('0x')) {
     const payload = input.slice(2);
-    const padded = payload.length % 2 === 0 ? payload : [0, ...payload].join('');
+    const padded = payload.length % 2 === 0 ? payload : '0' + payload;
     const hexArr: any[] = padded.match(/.{1,2}/g).map((x) => '0x' + x);
     const first = parseInt('0x80', 16) + hexArr.length;
-    console.log('hexArr', hexArr);
     return Uint8Array.from([first, ...hexArr]);
   }
 
@@ -63,28 +62,36 @@ function encode(input: Input): Uint8Array {
     }
     // If a string of more than 55-byte length
     else if (input.length > 55) {
-      const length = input.length;
-      const ByteAmountToStoreLengthValue = removePrefix(input)
-        .match(/.{1,2}/g)!
-        .length.toString(16)
-        .match(/.{1,2}/g)!.length;
-      const first = parseInt('0xb7', 16) + ByteAmountToStoreLengthValue;
+      const lengthInBytes = removePrefix(input).length.toString(16);
+      const ByteAmountToStoreLengthValue: any =
+        lengthInBytes.length % 2 === 0 ? lengthInBytes.match(/.{1,2}/g)! : ('0' + lengthInBytes).match(/.{1,2}/g)!;
+      const first = parseInt('0xb7', 16) + ByteAmountToStoreLengthValue.length;
       const encodedInput = input.split('').map((c) => c.charCodeAt(0));
-      return Uint8Array.from([first, length, ...encodedInput]);
+      return Uint8Array.from([first, ...ByteAmountToStoreLengthValue, ...encodedInput]);
     }
   } else if (Array.isArray(input)) {
-    const output: Uint8Array[] = [];
+    const payloadEncoding = [];
     let payloadLength = 0;
+
     for (let element of input) {
       const encoded = encode(element);
+      payloadEncoding.push(...encoded);
       payloadLength += encoded.length;
     }
 
     // if a list of 0-55 bytes long
     if (payloadLength <= 55) {
+      const first = parseInt('0xc0', 16) + payloadLength;
+      return Uint8Array.from([first, ...payloadEncoding]);
     }
 
     // if a list of greater than 55-byte length
+    if (payloadLength > 55) {
+      const lengthInHex = payloadLength.toString(16);
+      const ByteAmountToStoreLengthValue = hexToHexArray(lengthInHex);
+      const first = parseInt('0xf7', 16) + ByteAmountToStoreLengthValue.length;
+      return Uint8Array.from([first, ...ByteAmountToStoreLengthValue, ...payloadEncoding]);
+    }
   }
 
   return Uint8Array.from([]);
@@ -115,13 +122,72 @@ function encode(input: Input): Uint8Array {
 // console.log(encode([[], [[]], [[], [[]]]]));
 // console.log(encode('Lorem ipsum dolor sit amet, consectetur adipisicing elit'));
 
-console.log('\x04\x00', encode('\x04\x00'), RLP.encode('\x04\x00'));
-console.log('0x400', encode('0x400'), RLP.encode('0x400'));
-console.log("'\x00'", encode('\x00'), RLP.encode('\x00'));
-console.log("'0x7f7f'", encode('0x7f7f'), RLP.encode('0x7f7f'));
+const longstring =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris magna, suscipit sed vehicula non, iaculis faucibus tortor. Proin suscipit ultricies malesuada. Duis tortor elit, dictum quis tristique eu, ultrices at risus. Morbi a est imperdiet mi ullamcorper aliquet suscipit nec lorem. Aenean quis leo mollis, vulputate elit varius, consequat enim. Nulla ultrices turpis justo, et posuere urna consectetur nec. Proin non convallis metus. Donec tempor ipsum in mauris congue sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse convallis sem vel massa faucibus, eget lacinia lacus tempor. Nulla quis ultricies purus. Proin auctor rhoncus nibh condimentum mollis. Aliquam consequat enim at metus luctus, a eleifend purus egestas. Curabitur at nibh metus. Nam bibendum, neque at auctor tristique, lorem libero aliquet arcu, non interdum tellus lectus sit amet eros. Cras rhoncus, metus ac ornare cursus, dolor justo ultrices metus, at ullamcorper volutpat';
+
+// console.log('\x04\x00', encode('\x04\x00'), RLP.encode('\x04\x00'));
+// console.log('0x400', encode('0x400'), RLP.encode('0x400'));
+// console.log("'\x00'", encode('\x00'), RLP.encode('\x00'));
+// console.log('0x7f7f7f7', encode('0x7f7f7f7'), RLP.encode('0x7f7f7f7'));
+// console.log(`['cat', 'dog']`, encode(['cat', 'dog']), RLP.encode(['cat', 'dog']));
+// console.log(encode(['cat', 'dog']).toString() === RLP.encode(['cat', 'dog']).toString());
+// console.dir(encode(longstring), { maxArrayLength: null });
+// console.dir(RLP.encode(longstring), { maxArrayLength: null });
+// let me = encode(longstring);
+// let lib = RLP.encode(longstring);
+
+// let allEqual = false;
+
+// for (let i = 0; i < 1024; i++) {
+//   allEqual = lib[i] === me[i];
+// }
+
+// console.log('1st equality method', me.toString() === lib.toString());
+// console.log('2nd equality method', allEqual);
 
 // for (let t of cases) {
 //   // console.log(t, RLP.encode(t));
 // }
 
-export {};
+// let longlist = [
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+//   ['asdf', 'qwer', 'zxcv'],
+// ];
+
+// console.log(encode(longlist));
+
+// console.log(RLP.encode(longlist));
+
+// console.log('longlist:', encode(longlist).toString() === RLP.encode(longlist).toString());
+
+export { encode };
